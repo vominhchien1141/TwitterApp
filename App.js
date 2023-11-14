262775ed453e645282d5ee8ecc61b79065a154af
vominhchien1141 <74865427+vominhchien1141@users.noreply.github.com>
2023-11-15 02:52:08 +0700

    ReactX

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+import Login from './component/Login';
+import Home from './component/Home';
+
+
+const Stack = createNativeStackNavigator();
+
+
+// function MyStack(){
+//   return(
+    
+    
+//   );
+
+// }
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+       <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
+      <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
